aa3cef93e92b0a0bd06fe8cdd28f1a09aaa4692b
MennoLooman <majlooman@gmail.com>
2021-09-28 16:24:38 +0200

    small results from only pendulum

--- only_pen_parameter_estimation_grey_box.m
+++ only_pen_parameter_estimation_grey_box.m
@@ -1,28 +1,32 @@
 %% Load data
-load('data/run4.mat');
+load('data/run8.mat');
 t = u(:,1);
 h = t(2)-t(1);
 u = u(:,2);
-y = y;
+y = y(:,1);
 data = iddata(y,u,h); % data file used for grey_est and pem
 %linearization;
 clearvars -except data u y t h Ac Bc
 
 %% parameter estimate
-r_p_I = 0.129/2;
-d_p_I = 0.0366;
+r_p_I = 0.08063;
+d_p_I = 3.685e-05; %3.685e-05
 m_p_I = 0.024;
-g_I = 9.81;
+g_I = 9.8125; %9.812
+
 %% Get linearized system
 %guess physical parameters (stable equi)
 parameters = {'r_p',r_p_I;'d_p',d_p_I;'m_p',m_p_I;'g',g_I};
-init_sys = idgrey(@sys_matrices,parameters,'c');%,{},0);
+init_sys = idgrey(@sys_matrices,parameters,'c');
+% 
+% parameters2 = {'g1',-d_p_I/(m_p_I*r_p_I^2) ;'g2',-g_I/r_p_I};
+% init_sys = idgrey(@sys_matrices2,parameters2,'c');
 
 %% Identification
 %interesting options: 'EnforceStability' 'MaxIterations' 
-opt_id = greyestOptions('InitialState','zero','Display','on');
-%opt_id.Regularization.Lambda = 1;
-opt_id.Regularization.R = [1e5, 1e5,1e-3,1,1,1e5,1];
+opt_id = greyestOptions('Display','on');
+%opt_id.Regularization.Lambda = 1e-5;
+opt_id.Regularization.R = [1e3, 1, 1e2, 1e3];
 %opt_id.Regularization.Nominal = 'model';
 %opt_id.OutputWeight = [1e-5 0; 0 1e5];
 
@@ -43,5 +47,16 @@ function [A,B,C,D] = sys_matrices(r_p,d_p,m_p,g,Ts) %Ts is needed for greyest
          1                0    ];
     B = [0;0];
     C = [0 1];
-    D = [0;0];
+    D = [0];
+    if Ts>0
+       A = expm(Ts*A);
+    end
+end
+
+function [A,B,C,D] = sys_matrices2(g1,g2,Ts) %Ts is needed for greyest
+    A = [g1 g2;
+         1  0 ];
+    B = [0;0];
+    C = [0 1];
+    D = [0];
 end
